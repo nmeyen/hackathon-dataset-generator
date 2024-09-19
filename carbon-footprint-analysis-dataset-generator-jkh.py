77a54c560409b2e98d3495f31931f1372a43d9e7
nmeyen <nuzhim@hotmail.com>
2024-09-19 17:30:04 +0530

    V2 Data

--- carbon-footprint-analysis-dataset-generator-jkh.py
+++ carbon-footprint-analysis-dataset-generator-jkh.py
@@ -12,10 +12,12 @@ districts = [
 ]
 
 # Categories and subcategories for John Keells Holdings PLC
-categories = [
+categories1 = [
     'Transportation', 'Leisure', 'Property', 'Consumer Foods', 'Retail',
     'Financial Services', 'Information Technology', 'Plantation Services'
 ]
+
+categories = ['Transportation', 'Warehousing', 'Manufacturing']
 
 subcategories = {
     'Transportation': ['Ports & Shipping', 'Logistics'],
@@ -50,10 +52,10 @@ for _ in range(600):
     delta = end_date - start_date
     random_days = random.randint(0, delta.days)
     date = (start_date + timedelta(days=random_days)).strftime('%Y-%m-%d')
-    
     category = np.random.choice(categories)
-    subcategory = np.random.choice(subcategories[category])
-    company = np.random.choice(companies[category])
+    category1 = np.random.choice(categories1)
+    subcategory = np.random.choice(subcategories[category1])
+    company = np.random.choice(companies[category1])
     district = np.random.choice(districts)
     emissions = np.round(np.random.uniform(100, 1200), 2)
     percentage_of_total = np.round(np.random.uniform(1, 25), 2)
@@ -65,4 +67,4 @@ for _ in range(600):
 df_carbon = pd.DataFrame(data, columns=[
     'Date', 'Category', 'Subcategory', 'Company', 'District', 'Emissions', 'Percentage_of_Total'
 ])
-df_carbon.to_csv('carbon_footprint_analysis_jkh.csv', index=False)
+df_carbon.to_csv('carbon_footprint_analysis_jkh_v2.csv', index=False)
